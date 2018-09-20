41bcb39f113c2af539af8bca4bed790c9b0d076b
Pfollvosten <lukas.boehm00@gmx.de>
2018-09-20 21:06:13 +0200

    python behindert.

--- Client/MainClient.py
+++ Client/MainClient.py
@@ -4,5 +4,6 @@ from ClientSocket import ClientSocket
 sock = ClientSocket()
 
 from GPIOReader import GPIOReader
-gpio = GPIOReader()
-gpio.start()
+t_gpio = GPIOReader()
+t_gpio.setName("t_gpio")
+t_gpio.start()
