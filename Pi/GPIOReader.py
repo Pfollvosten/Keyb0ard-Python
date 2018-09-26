cc36a2b3330bd5be402a43d2d5f65136212958c8
Pfollvosten <lukas.boehm00@gmx.de>
2018-09-26 19:49:09 +0200

    gpio logic

--- Pi/GPIOReader.py
+++ Pi/GPIOReader.py
@@ -3,7 +3,8 @@
 from gpiozero import Button
 
 pressed = [False] * 8
-
+blocked = False
+ticks_since = 0
 buttons = [
     Button(26),
     Button(19),
@@ -19,17 +20,33 @@ def read_keys():
     """
     scanning every button if pressed. Gets called from ClientSocket.run
     """
+    ticks_since += 1 if blocked else ticks_since = 0
+
     for but in range(len(buttons)):
-        if buttons[but].is_pressed:
-            pressed[but] = True
+        if buttons[but].is_pressed and not blocked:
+            blocked = True
             print("pressed button: " , but)
-        else:
+            if ticks_since <= 250:
+                blocked = True
+                pressed[but] = True
+            if ticks_since >= 250 and ticks_since%50==0:
+                pass
+            
+        elif buttons[but].is_released:
             pressed[but] = False
+            blocked = False
     return pressed
 
-# def read_keys(self):
-#     import random
-#     ran = random.randint(0,7)
-#     self.pressed = [False] * 8
-#     self.pressed[ran] = True
-#     return self.pressed
+
+
+# def read_keys_alt():
+#     """
+#     scanning every button if pressed. Gets called from ClientSocket.run
+#     """
+#     for but in range(len(buttons)):
+#         if buttons[but].is_pressed:
+#             pressed[but] = True
+#             print("pressed button: " , but)
+#         else:
+#             pressed[but] = False
+# return pressed
