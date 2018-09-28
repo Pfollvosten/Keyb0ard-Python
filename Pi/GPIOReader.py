9b0ff4f291cd5da586e58ff56f7145172d77c3ff
Pfollvosten <lukas.boehm00@gmx.de>
2018-09-29 01:49:31 +0200

    output

--- Pi/GPIOReader.py
+++ Pi/GPIOReader.py
@@ -12,38 +12,38 @@ buttons = [
     [Button(12), False, 0]
 ]
 
-pressed = [False] * 8
-
-# def read_keys():
-#     """
-#     scanning every button if pressed. Gets called from ClientSocket.run()
-#     """
-#     ret = [False] * 8
-#     for but in range(len(buttons)):
-#         with buttons[but] as b:
-#             # send out 1 stroke then block it for 300ms
-#             if b[2] == 0:
-#                 b[1] = True
-#             elif b[1]:
-#                 b[1] = False
-#                 b[2] += 1
-#                 if b[2] >= 30:
-#                     b[1] = True
-#             elif b[0].is_released:
-#                 b[1] = False
-#                 b[2] = 0
-#             ret[but] = b[1]
-#     return ret
-    # return list([b[1] for b in buttons])
-    
 def read_keys():
     """
-    scanning every button if pressed. Gets called from ClientSocket.run
+    scanning every button if pressed. Gets called from ClientSocket.run()
     """
+    ret = []
     for but in range(len(buttons)):
-        if buttons[but].is_pressed:
-            pressed[but] = True
-            print("pressed button: " , but)
-        else:
-            pressed[but] = False
-    return pressed
+        with buttons[but] as b:
+            # send out 1 stroke then block it for 300ms
+            if b[2] == 0:
+                b[1] = True
+            elif b[1]:
+                b[1] = False
+                b[2] += 1
+                if b[2] >= 30:
+                    b[1] = True
+            elif b[0].is_released:
+                b[1] = False
+                b[2] = 0
+            ret.append(b[1])
+    ret = list(ret)
+    print(ret)
+    return ret
+    # return list([b[1] for b in buttons])
+    
+# def read_keys():
+#     """
+#     scanning every button if pressed. Gets called from ClientSocket.run
+#     """
+#     for but in range(len(buttons)):
+#         if buttons[but].is_pressed:
+#             pressed[but] = True
+#             print("pressed button: " , but)
+#         else:
+#             pressed[but] = False
+#     return pressed
