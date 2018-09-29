f93342cdb8e9e7cd18f03b48ef085a73d82772cf
Pfollvosten <lukas.boehm00@gmx.de>
2018-09-29 22:56:05 +0200

    flax mich nicht

--- Pi/GPIOReader.py
+++ Pi/GPIOReader.py
@@ -17,21 +17,19 @@ def read_keys():
     scanning every button if pressed. Gets called from ClientSocket.run()
     """
     for but in range(len(buttons)):
-        print("for problem")
-        with buttons[but] as b:
-            print("with problem")
-            # send out 1 stroke then block it for 300ms
-            if b[0].is_pressed:
-                print("pressed problem")
-                if b[2] == 0:
-                    b[1] = True
-                elif b[1]:
-                    b[1] = False
-                    b[2] += 1
-                    if b[2] >= 30:
-                        b[1] = True
-            elif b[0].is_released:
+        # with buttons[but] as b:
+        b = buttons[but]
+        # send out 1 stroke then block it for 300ms
+        if b[0].is_pressed:
+            if b[2] == 0:
+                b[1] = True
+            elif b[1]:
                 b[1] = False
-                b[2] = 0
+                b[2] += 1
+                if b[2] >= 30:
+                    b[1] = True
+        elif b[0].is_released:
+            b[1] = False
+            b[2] = 0
     # return pressed state for every button as a list            
     return list([bu[1] for bu in buttons])
